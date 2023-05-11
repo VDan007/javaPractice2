824e614b893967e4207c117aa3c4ce81651d4c31
DanielVarjaskéri <varjaskerid@gmail.com>
2023-05-11 14:30:57 +0200

    image draggable in progress

--- Practice/src/Drag_DropFrame.java
+++ Practice/src/Drag_DropFrame.java
@@ -1,11 +1,18 @@
 import javax.swing.JFrame;
 
 public class Drag_DropFrame extends JFrame {
+
+    Drag_Panel panel;
     
     Drag_DropFrame(){
+
+        panel = new Drag_Panel();
+
+        this.add(panel);
         this.setTitle("Drag & Drop Demo");
         this.setSize(600, 600);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLocationRelativeTo(null);
         this.setVisible(true);
     }
 }
--- Practice/src/Drag_Panel.java
+++ Practice/src/Drag_Panel.java
@@ -1,5 +1,27 @@
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+import java.awt.Graphics;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseMotionAdapter;
 
+public class Drag_Panel extends JPanel  {
 
-public class Drag_Panel  {
+    ImageIcon image = new ImageIcon("star.png");
     
+    Drag_Panel(){
+
+        
+    }
+
+    public void paintComponent(Graphics g){
+
+    }
+
+    private class ClickListener extends MouseAdapter{
+
+    }
+
+    private class DragListener extends MouseMotionAdapter{
+        
+    }
 }
